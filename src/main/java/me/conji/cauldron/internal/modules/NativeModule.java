812e3483095365ab31791b8a4f55e47080dcd141
Justin Cox <jcox2@wayfair.com>
2020-01-27 16:27:18 -0500

    Try guava for reflection

--- src/main/java/me/conji/cauldron/internal/modules/NativeModule.java
+++ src/main/java/me/conji/cauldron/internal/modules/NativeModule.java
@@ -1,24 +1,11 @@
 package me.conji.cauldron.internal.modules;
 
 import me.conji.cauldron.api.Module;
-import me.conji.cauldron.core.Isolate;
 
 import me.conji.cauldron.api.JsAccess;
 
 @JsAccess.INNER_BINDING("native_module")
 public abstract class NativeModule extends Module {
-  static boolean exists(String id) {
-    return getCached(id) != null;
-  }
-
-  static NativeModule getCached(String id) {
-    return (NativeModule) Isolate.activeIsolate().getModuleManager().getInternalModule(id);
-  }
-
-  static String wrapBody(String body) {
-    return "(function (exports, module, require, process, plugin, NativeModule) {" + body + "\n})";
-  }
-
   @Override
   public boolean isInternal() {
     return true;
